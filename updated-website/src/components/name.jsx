0337ff93c48a9a54824cd61b0bb310fcdde0fb68
Ashley Zhao <zhaoashley2005@gmail.com>
2023-08-11 12:59:35 -0400

    set up enviro and test animation

---
+++ updated-website/src/components/name.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Lottie from "lottie-react"
+import animationData from "../assets/animation_ll6sohn8.json"
+
+export default function name(){
+    return(
+        <div>
+            <h1>Hi, I'm <i>Ashley Zhao</i></h1>
+            <Lottie animationData={animationData}/>
+        </div>   
+    )
+}
